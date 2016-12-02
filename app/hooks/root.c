cccc765b38b86743075984dd92449ab889bcbf40
Rémy MAUGEY <remy.maugey@etu.u-bordeaux.fr>
2016-12-02 06:00:48 +0100

    Test

--- app/hooks/root.c
+++ app/hooks/root.c
@@ -1,7 +1,10 @@
 #include "hooks.h"
+#include <views.h>
 
 int hook_root (HTTP_header_t *header) {
 	HTTP_header_set(header, CONTENT_TYPE, "text/html");
 	HTTP_header_send(header);
+	VIEW(view_asso,
+		"HELLO WORLD!", "Boisson, poisson", "Voici une superbe description\net multiligne en plus", "ex@example.com", "06", "127 chemin de la poire", "http://lafistinière.com");
 	return 200;	
 }
