76cde07e6b13b5d72420f16a2c203a4916f21835
Rémy MAUGEY <remy.maugey@etu.u-bordeaux.fr>
2016-12-02 05:47:17 +0100

    hook new asso

--- app/hooks/register.c
+++ app/hooks/register.c
@@ -1,7 +1,9 @@
 #include "hooks.h"
+#include <views.h>
 
 int hook_register (HTTP_header_t *header) {
 	HTTP_header_set(header, CONTENT_TYPE, "text/html");
 	HTTP_header_send(header);
+	VIEW(new_asso);
 	return 200;
 }
