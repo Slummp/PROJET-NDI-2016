09c47dba91a3256e4fb86048f25d24aa07b0fea0
Rémy MAUGEY <remy.maugey@etu.u-bordeaux.fr>
2016-12-02 06:40:46 +0100

    qdsg

--- app/hooks/root.c
+++ app/hooks/root.c
@@ -5,6 +5,6 @@ int hook_root (HTTP_header_t *header) {
 	HTTP_header_set(header, CONTENT_TYPE, "text/html");
 	HTTP_header_send(header);
 	VIEW(view_asso,
-		"HELLO WORLD!", "Boisson, poisson", "Voici une superbe description\net multiligne en plus", "ex@example.com", "06", "127 chemin de la poire", "http://lafistinière.com");
+		"HELLO WORLD!", "Boisson, poisson", "Voici une superbe description\net multiligne en plus", "ex@example.com", "06", "127 chemin de la poire", "http://lafistinière.com", "http://facebook.com", "");
 	return 200;	
 }
